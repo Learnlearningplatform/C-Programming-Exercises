0d30b44aeb8b483efb7610ebc413e28a66c3fdfd
TeAmP0is0N <rohitrathore.imh55@gmail.com>
2019-09-28 13:49:55 +0530

    Added

---
+++ sum_of_two_digits.c
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+
+int main()
+{
+    int A = 0;
+    int  B =0;
+    int sum = 0;
+
+
+    printf("Enter two numbers A and B : \n");
+
+
+    scanf("%d%d", &A, &B);
+
+    sum = A + B;
+    printf("Sum of A and B is: %d", sum);
+
+    return 0;
+}
